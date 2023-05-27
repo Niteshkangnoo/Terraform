d6b0fec9da989f6d37d029c6b189da14ad309f23
Nitesh Kangnoo <niteshkangnoo@gmail.com>
2023-05-27 20:38:55 +0000

    Added Backend to save state File

--- provider.tf
+++ provider.tf
@@ -5,9 +5,20 @@ terraform {
       version = "3.57.0"
     }
   }
+
+  backend "azurerm" {
+  resource_group_name = "appgrp"
+  storage_account_name = "tfstrgaccnt113"
+  container_name = "tfstate"
+  key = "terraform.tfstate"
+  #sas_token = "SAS_token_value"
+}
+
 }
 
 provider "azurerm" {
   features {}
   skip_provider_registration = true
 }
+
+
