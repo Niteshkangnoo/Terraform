304bde3d4f442edef46f10d8855df8a36518fbac
Nitesh Kangnoo <niteshkangnoo@gmail.com>
2023-05-27 14:00:20 +0000

    Adding NSG

--- networksecuritygroup.tf
+++ networksecuritygroup.tf
@@ -4,3 +4,10 @@ resource "azurerm_network_security_group" "tfnsg" {
   resource_group_name = azurerm_resource_group.appgrp.name
   depends_on          = [azurerm_resource_group.appgrp]
 }
+
+resource "azurerm_network_security_group" "testnsg" {
+  name                = "testnsg"
+  location            = azurerm_resource_group.appgrp.location
+  resource_group_name = azurerm_resource_group.appgrp.name
+  depends_on          = [azurerm_resource_group.appgrp]
+}
